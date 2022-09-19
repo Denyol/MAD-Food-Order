04678afc7518becde7a68e8675890d0835c3a9b1
Daniel Tucker <Denyol@users.noreply.github.com>
2022-09-19 19:39:42 +0800

    App saves state of nav bar on rotate or activity change

--- app/src/main/java/edu/curtin/danieltucker/foode/MainActivity.java
+++ app/src/main/java/edu/curtin/danieltucker/foode/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.navigation.NavigationBarView;
@@ -15,35 +16,56 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
 
     private SQLiteDatabase database;
     private FragmentManager fm;
+    private NavigationBarView navBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NavigationBarView navBar = findViewById(R.id.navBar);
+        navBar = findViewById(R.id.navBar);
 
         database = new DBHelper(this).getWritableDatabase();
 
         fm = getSupportFragmentManager();
 
-        fm.beginTransaction().replace(R.id.mainFrameLayout, RestaurantsFragment.class, null). commit();
+        int lastNav = -1;
+
+        if (savedInstanceState != null) {
+            lastNav = savedInstanceState.getInt("NAV_CURRENT", -1);
+            switchFragment(lastNav);
+            navBar.setSelectedItemId(lastNav);
+        }
+
+        if (lastNav == -1)
+            fm.beginTransaction().replace(R.id.mainFrameLayout, RestaurantsFragment.class, null). commit();
 
         navBar.setOnItemSelectedListener(this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
+        switchFragment(item.getItemId());
+        return true;
+    }
+
+    private void switchFragment(int navMenuItemId) {
+        switch (navMenuItemId) {
             case R.id.basket:
                 Log.d("MainActivity", "Switching to basket fragment");
                 fm.beginTransaction().replace(R.id.mainFrameLayout, BasketFragment.class, null).commit();
                 break;
             case R.id.restaurants:
                 Log.d("MainActivity", "Switching to restaurants fragment");
-                fm.beginTransaction().replace(R.id.mainFrameLayout, RestaurantsFragment.class, null). commit();
+                fm.beginTransaction().replace(R.id.mainFrameLayout, RestaurantsFragment.class, null, "R"). commit();
                 break;
         }
-        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt("NAV_CURRENT", navBar.getSelectedItemId());
+        Log.d("MainActivity", "Saving state " + navBar.getSelectedItemId());
+        super.onSaveInstanceState(outState);
     }
 }
